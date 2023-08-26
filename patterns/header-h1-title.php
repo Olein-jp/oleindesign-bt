27cda996121216420bf459e605ae5a0463667027
Olein-jp <koji.kuno@gmail.com>
2023-08-26 17:05:34 +0900

    fixed header parts

--- patterns/header-h1-title.php
+++ patterns/header-h1-title.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Title: header
- * Slug: /header
+ * Title: H1 Title Header
+ * Slug: oleindesign/header/header-h1-title
  * Categories: hidden
  * Inserter: no
  */
@@ -9,11 +9,11 @@
 <!-- wp:group {"tagName":"header","style":{"spacing":{"padding":{"top":"var:preset|spacing|m","bottom":"var:preset|spacing|m","left":"var:preset|spacing|m","right":"var:preset|spacing|m"}}},"className":"site-header","layout":{"type":"default"}} -->
 <header class="wp-block-group site-header" style="padding-top:var(--wp--preset--spacing--m);padding-right:var(--wp--preset--spacing--m);padding-bottom:var(--wp--preset--spacing--m);padding-left:var(--wp--preset--spacing--m)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group"><!-- wp:image {"id":6177,"width":350,"sizeSlug":"full","linkDestination":"custom"} -->
-<figure class="wp-block-image size-full is-resized"><a href="http://olein-design.wp/"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/olein-design-logo.svg" alt="<?php echo esc_attr_e( 'オレインデザイン', 'oleindesign' ); ?>" class="wp-image-6177" style="width:350px" width="350"/></a></figure>
+<figure class="wp-block-image size-full is-resized"><a href="http://olein-design.wp/"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/olein-design-logo.svg" alt="<?php echo esc_attr_e( 'オレインデザイン', 'Olein Design Block Theme' ); ?>" class="wp-image-6177" style="width:350px" width="350"/></a></figure>
 <!-- /wp:image -->
 
-<!-- wp:heading {"className":"u-sr-only"} -->
-<h2 class="wp-block-heading u-sr-only">オレインデザイン</h2>
+<!-- wp:heading {"level":1,"className":"u-sr-only"} -->
+<h1 class="wp-block-heading u-sr-only">オレインデザイン</h1>
 <!-- /wp:heading -->
 
 <!-- wp:navigation {"ref":6154,"style":{"spacing":{"blockGap":"var:preset|spacing|s"}},"fontSize":"xs"} /--></div>
